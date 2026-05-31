89a3da47cc6daa2acdb208824260a514afdb6729
Harshit-ai-ai <harshit.17apr@gmail.com>
2026-05-31 14:50:47 +0530

    main change

--- smart-classroom/backend/main.py
+++ smart-classroom/backend/main.py
@@ -97,3 +97,14 @@ async def attendance(
             students
         )
     }
+
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port
+    )
